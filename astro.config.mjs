5927bd0e4e51ef0bc53ce0dd7fedcd254f81a2bd
Joseph Lilleberg <jjglillebergds@gmail.com>
2026-02-28 00:37:35 -0600

    setup: add integrations and project structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://josephlilleberg.com",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap(), mdx()],
+});
